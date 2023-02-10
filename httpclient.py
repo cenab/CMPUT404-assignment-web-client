3c0eff9d76a76a854367540fbc45cb47ba430238
Cenab Batu Bora <40250311+cenab@users.noreply.github.com>
2023-02-10 15:42:42 -0700

    assignment submission ready commit

--- httpclient.py
+++ httpclient.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # coding: utf-8
-# Copyright 2016 Abram Hindle, https://github.com/tywtyw2002, and https://github.com/treedust
+# Copyright 2016 Abram Hindle, https://github.com/tywtyw2002, and https://github.com/treedust, Cenab Batu Bora https://github.com/cenab
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -24,6 +24,14 @@ import re
 # you may use urllib to encode data appropriately
 import urllib.parse
 
+PORT = 80 #default port
+
+CONTENT_TYPE = "application/x-www-form-urlencoded"
+#USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.3 Safari/605.1.15"
+#https://user-agents.net/string/mozilla-5-0-x11-ubuntu-linux-x86-64-rv-96-0-gecko-20100101-firefox-96-0
+USER_AGENT = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:96.0) Gecko/20100101 Firefox/96.0"
+
+
 def help():
     print("httpclient.py [GET/POST] [URL]\n")
 
@@ -33,21 +41,45 @@ class HTTPResponse(object):
         self.body = body
 
 class HTTPClient(object):
-    #def get_host_port(self,url):
+    def parse_url(self,url):
+        #https://stackoverflow.com/questions/50499273/urlparse-fails-with-simple-url
+        try:
+            #parse the url and check with failsafe
+            if not re.search(r'^[A-Za-z0-9+.\-]+://', url):
+                url = 'http://{0}'.format(url)
+            url_netloc = urllib.parse.urlparse(url).netloc
+            #domain or ip address
+            if ":" in url_netloc:
+                host = url_netloc.split(":")[0]
+                port = int(url_netloc.split(":")[1])
+            else:
+                host = socket.gethostbyname(url_netloc)
+                port = PORT
+            return host, port, urllib.parse.urlparse(url)
+        except:
+            return None, None, None
 
     def connect(self, host, port):
+        # establish a socket connection and connect to the host
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.connect((host, port))
-        return None
+        socket_status = self.socket.connect((host, port))
+        return None socket_status
 
     def get_code(self, data):
-        return None
+        try:
+            code = int(data.split()[1])
+        except:
+            code = None
+        return code
 
     def get_headers(self,data):
-        return None
+        return None # you need to implement this!
 
     def get_body(self, data):
-        return None
+        try:
+            return data.split("\r\n")[-1]
+        except:
+            return ""
     
     def sendall(self, data):
         self.socket.sendall(data.encode('utf-8'))
@@ -70,11 +102,89 @@ class HTTPClient(object):
     def GET(self, url, args=None):
         code = 500
         body = ""
+
+        host, port, url_parsed = self.parse_url(url)
+        path = url_parsed.path
+
+        if host == None:
+            return HTTPResponse(404, None)
+        elif port == None:
+            return HTTPResponse(404, None)
+
+        if self.connect(host, port) == 0:
+            raise Exception('Ehem ehem, socket failed to connect! Probably there is problem with the way the user is running.')
+
+
+        if path == "": path = "/"
+
+        query = url_parsed.query
+
+        if args != None:
+            #args is a dictionary
+            for arg in args:
+                if query == "": query += str(arg) + "=" + str(args[arg])
+                else: query += "&" + str(arg) + "=" + str(args[arg])
+
+        if query != "": query = "?" + query
+
+        headers = ""
+        headers += "HTTP/1.1\r\n"
+        headers += "Host: {}\r\n".format(url_parsed.netloc)
+        headers += "Accept: */*\r\n"
+        headers += "User-Agent: {}\r\n".format(USER_AGENT)
+        headers += "Connection: close\r\n"
+
+        self.sendall("GET {} {}\r\n".format(path + query, headers))
+        data = self.recvall(self.socket)
+
+        self.close()
+
+        print("Data for the request:\n", data, "\n**********************************************************")
+        body = self.get_body(data)
+        code = self.get_code(data)
         return HTTPResponse(code, body)
 
     def POST(self, url, args=None):
         code = 500
         body = ""
+
+        host, port, url_parsed = self.parse_url(url)
+        path = url_parsed.path
+
+        if host == None:
+            return HTTPResponse(404, None)
+        elif port == None:
+            return HTTPResponse(404, None)
+
+        if self.connect(host, port) == 0:
+            raise Exception('Ehem ehem, socket failed to connect! Probably there is problem with the way the user is running.')
+        
+        if path == "": path = "/"
+
+        post_data = ""
+
+        if args != None:
+            for arg in args:
+                if post_data == "": post_data += str(arg) + "=" + str(args[arg])
+                else: post_data += "&" + str(arg) + "=" + str(args[arg])
+
+        headers = ""
+        headers += "HTTP/1.1\r\n"
+        headers += "Host: {}\r\n".format(url_parsed.netloc)
+        headers += "Accept: */*\r\n"
+        headers += "Content-Type: {}\r\n".format(CONTENT_TYPE)
+        headers += "Content-Length: {}\r\n".format(len(post_data.encode('utf-8')))
+        headers += "User-Agent: {}\r\n".format(USER_AGENT)
+        headers += "Connection: close\r\n"
+
+        self.sendall("POST {} {}\r\n{}".format(path, headers, post_data))
+        data = self.recvall(self.socket)
+
+        self.close()
+
+        print("Data for the request:\n", data, "\n**********************************************************")
+        body = self.get_body(data)
+        code = self.get_code(data)
         return HTTPResponse(code, body)
 
     def command(self, url, command="GET", args=None):
